7b4b617634683cc7135d94a2ceeb738f71c0a120
Михаил <solomein-m@mail.ru>
2025-04-28 20:57:34 +0500

    refactor

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import cv2
+from config import API_KEY
 import io
 from io import BytesIO
 import base64
@@ -13,57 +14,58 @@ from PIL.ImageFile import ImageFile
 #from config import API_KEY
 
 # Подготовка данных (разделение видео на кадры)
-def extract_frames(video_path):
-    vidcap = cv2.VideoCapture(video_path)
-    frames = []
-    while True:
-        success, image = vidcap.read()
-        if not success:
-            break
-        frames.append(image)
-    return frames;
+# def extract_frames(video_path):
+#     vidcap = cv2.VideoCapture(video_path)
+#     frames = []
+#     while True:
+#         success, image = vidcap.read()
+#         if not success:
+#             break
+#         frames.append(image)
+#     return frames
+#
+#
+# image_path = "C:\\Users\\solom\\Downloads\\1.MOV"
+# frames = extract_frames(image_path)
+#
+# if frames:
+#     for i in range(len(frames)):
+#         # Выводим первый кадр на экран
+#         cv2.imshow('Frame', frames[i])  # Здесь можно указать индекс нужного кадра
+#
+#         # Ожидаем нажатия клавиши
+#         cv2.waitKey(0)  # 0 означает, что программа будет ждать бесконечно, пока не будет нажата клавиша
+#
+#         # Закрываем все окна
+#         cv2.destroyAllWindows()
+# else:
+#     print("Не удалось извлечь кадры из видео.")
+# print(frames)
+image_path = "C:\\Users\\solom\\PycharmProjects\\CabelsAnalyzer\\data\\defects\\32-31 (79 м) Фонарь и обрыв проволоки.png"
+with open(image_path, "rb") as image_file:
+    image_bytes = image_file.read()
+image = Image.open(io.BytesIO(image_bytes))
 
+client = InferenceHTTPClient(
+    api_url="https://detect.roboflow.com",
+    api_key=API_KEY
+)
 
-image_path = "C:\\Users\\solom\\Downloads\\1.MOV"
-frames = extract_frames(image_path)
+result = client.run_workflow(
+    workspace_name="cabelsanalyzer",
+    workflow_id="detect-count-and-visualize",
+    images={
+        "image": image
+    },
+    use_cache=True # cache workflow definition for 15 minutes
+)[0]
 
-if frames:
-    for i in range(len(frames)):
-        # Выводим первый кадр на экран
-        cv2.imshow('Frame', frames[i])  # Здесь можно указать индекс нужного кадра
+image_bytes = base64.b64decode(result["output_image"])
 
-        # Ожидаем нажатия клавиши
-        cv2.waitKey(0)  # 0 означает, что программа будет ждать бесконечно, пока не будет нажата клавиша
+# Создание изображения из байтов
+img = Image.open(BytesIO(image_bytes))
 
-        # Закрываем все окна
-        cv2.destroyAllWindows()
-else:
-    print("Не удалось извлечь кадры из видео.")
-print(frames)
-# with open(image_path, "rb") as image_file:
-#     image_bytes = image_file.read()
-# image = Image.open(io.BytesIO(image_bytes))
-#
-# client = InferenceHTTPClient(
-#     api_url="https://detect.roboflow.com",
-#     api_key="9gobrtw7bAnhMrmIZspP"
-# )
-#
-# result = client.run_workflow(
-#     workspace_name="cabelsanalyzer",
-#     workflow_id="detect-count-and-visualize",
-#     images={
-#         "image": image
-#     },
-#     use_cache=True # cache workflow definition for 15 minutes
-# )[0]
-#
-# image_bytes = base64.b64decode(result["output_image"])
-#
-# # Создание изображения из байтов
-# img = Image.open(BytesIO(image_bytes))
-#
-# # Отображение изображения
-# img.show()
-#
-# print(result)
+# Отображение изображения
+img.show()
+
+print(result)
--- second.py
+++ second.py
@@ -1,9 +1,11 @@
 from flask import Blueprint, render_template, session, flash, redirect, url_for, request
 from models import db, Users
+from functools import wraps
 
 second = Blueprint("second", __name__, static_folder="static", template_folder="templates")
 
 def login_required(f):
+    @wraps(f)
     def decorated_function(*args, **kwargs):
         if 'user' not in session:
             flash("You need to log in first!", "warning")
@@ -12,32 +14,30 @@ def login_required(f):
     return decorated_function
 
 @second.route("/admin")
-#@login_required
+@login_required
 def home():
     return "This is an admin route that requires login."
 
-@login_required
 @second.route("/user", methods=["GET", "POST"])
+@login_required
 def user():
-    return "Users"
-    # email = None
-    # if "user" in session:
-    #     user = session["user"]
-    #
-    #     if request.method == "POST":
-    #         email = request.form["email"]
-    #         session["email"] = email
-    #         found_user = Users.query.filter_by(name=user).first()
-    #         found_user.email = email
-    #         db.session.commit()
-    #         flash("Email was saved")
-    #     else:
-    #         if "email" in session:
-    #             email = session["email"]
-    #     return render_template("user.html", email="dog")
+    email = None
+    if "user" in session:
+        user = session["user"]
 
-# @second.route("/view")
-# @login_required
-# def view():
-#     return render_template("view.html", values=Users.query.all())
-#
+        if request.method == "POST":
+            email = request.form["email"]
+            session["email"] = email
+            found_user = Users.query.filter_by(name=user).first()
+            found_user.email = email
+            db.session.commit()
+            flash("Email was saved")
+        else:
+            if "email" in session:
+                email = session["email"]
+        return render_template("user.html", email="dog")
+
+@second.route("/view")
+@login_required
+def view():
+    return render_template("view.html", values=Users.query.all())
