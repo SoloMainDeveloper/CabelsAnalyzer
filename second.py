d29436fb4675b5ceddbb187151e11b4184401928
Михаил <solomein-m@mail.ru>
2025-05-06 18:05:28 +0500

    fix

--- second.py
+++ second.py
@@ -3,7 +3,6 @@ from models import db, Users, Reports
 from functools import wraps
 import numpy as np
 from videoanalyzer import process_video
-import bcrypt
 
 second = Blueprint("second", __name__, static_folder="static", template_folder="templates")
 
@@ -15,44 +14,6 @@ def login_required(f):
             return redirect(url_for("login"))
         return f(*args, **kwargs)
     return decorated_function
-
-@second.route("/login", methods=["GET", "POST"])
-def login():
-    if request.method == "POST":
-        session.permanent = True
-        user = request.form["login"]
-        password = request.form["password"]
-
-        found_user = Users.query.filter_by(name=user).first()
-        if not found_user:
-            hashed_password = hash_password(password)
-            usr = Users(name=user, password=hashed_password)
-            db.session.add(usr)
-            db.session.commit()
-            session["user"] = user
-            flash("Аккаунт создан. Добро пожаловать, " + user)
-            return redirect(url_for("second.home"))
-        else:
-            if check_password(found_user.password, password):
-                session["user"] = user
-                flash("Login successfully!")
-                return redirect(url_for("second.home"))
-            else:
-                flash("Пароль неверный.")
-                return redirect(url_for("login"))
-    else:
-        if "user" in session:
-            flash("Already logged in!")
-            return redirect(url_for("second.home"))
-        return render_template("login.html")
-
-@second.route("/logout")
-def logout():
-    if "user" in session:
-        session.pop("user", None)
-        session.pop("email", None)
-        flash("You have been logged out!", "info")
-    return redirect(url_for("login"))
 
 @second.route("/")
 @second.route("/home")
@@ -140,10 +101,4 @@ def download_report(report_name):
 
     return response
 
-def hash_password(password):
-    salt = bcrypt.gensalt()
-    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
-    return hashed_password
 
-def check_password(hashed_password, password):
-    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
